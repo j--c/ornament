270f1becd1636f4d22357bd8a72818d9eb6bbd0d
j--c <johnmichaelcollins@protonmail.com>
2024-11-12 08:50:49 -0500

    added data packing to helper, updated test helper accordingly

--- ornament/helper.py
+++ ornament/helper.py
@@ -1,5 +1,10 @@
 from io import BufferedIOBase
 from os.path import exists
+import struct
+
+STR_ENCODING = 'utf-8'
+LNG_MAX = 9223372036854775807
+INT_MAX = 1073741824
 
 
 def open_binary_file(path: str, mode: str) -> BufferedIOBase:
@@ -8,3 +13,62 @@ def open_binary_file(path: str, mode: str) -> BufferedIOBase:
     else:
         not_found_msg = f'File not found at: {path}!'
         raise FileNotFoundError(not_found_msg)
+
+
+# Strings -----------------------------------------
+def pack_s(s: str, encoding: str = None) -> bytes:
+    if encoding is None:
+        encoding = STR_ENCODING
+    s_encoded = s.encode(encoding)
+    s_len = len(s_encoded)
+    pack_format = f'@{s_len}s'
+    return struct.pack(pack_format, s_encoded)
+
+
+def unpack_s(data: bytes, encoding: str = None) -> str:
+    if encoding is None:
+        encoding = STR_ENCODING
+    pack_format = f'@{len(data)}s'
+    s_encoded = struct.unpack(pack_format, data)[0]
+    return s_encoded.decode(encoding)
+
+
+# Integers ----------------------------------------
+def pack_i(n: int) -> bytes:
+    pack_format = '@i'
+    if n < INT_MAX:
+        return struct.pack(pack_format, n)
+    else:
+        val_msg = f'pack_i requires value less than or equal to {INT_MAX}'
+        raise ValueError(val_msg)
+
+
+def unpack_i(data: bytes) -> int:
+    pack_format = '@i'
+    return struct.unpack(pack_format, data)[0]
+
+
+# Longs -------------------------------------------
+def pack_l(n: int) -> bytes:
+    pack_format = '@l'
+    if n < LNG_MAX:
+        return struct.pack(pack_format, n)
+    else:
+        val_msg = f'write_l requires value less than or equal to {LNG_MAX}'
+        raise ValueError(val_msg)
+
+
+def unpack_l(data: bytes) -> int:
+    pack_format = '@l'
+    return struct.unpack(pack_format, data)[0]
+
+
+# Booleans-----------------------------------------
+def pack_b(b: bool) -> bytes:
+    pack_format = '@?'
+    return struct.pack(pack_format, b)
+
+
+def unpack_b(data: bytes) -> bool:
+    pack_format = '@?'
+    return struct.unpack(pack_format, data)[0]
---
+++ test/test_helper.py
@@ -0,0 +1,84 @@
+from ornament.helper import unpack_s
+from ornament.helper import unpack_i
+from ornament.helper import unpack_l
+from ornament.helper import unpack_b
+from ornament.helper import pack_s
+from ornament.helper import pack_i
+from ornament.helper import pack_l
+from ornament.helper import pack_b
+import unittest
+
+
+class TestHelper(unittest.TestCase):
+
+    def test_pack_unpack_s(self) -> None:
+
+        # Given a string "the string"
+        s = 'the string'
+
+        # When I call pack_s on the string
+        # and call unpack_s on the packed string
+        packed_s = pack_s(s)
+        unpacked_s = unpack_s(packed_s)
+
+        # Then unpacked string equals the original string
+        self.assertEqual(unpacked_s, 'the string')
+
+    def test_pack_unpack_i(self) -> None:
+
+        # Given an integer
+        n = 73965
+
+        # When I call pack_i on the integer
+        # and call unpack_i on the packed integer
+        packed_n = pack_i(n)
+        unpacked_n = unpack_i(packed_n)
+
+        # Then unpacked integer equals the original integer
+        self.assertEqual(unpacked_n, 73965)
+
+    def test_pack_unpack_i_too_big(self) -> None:
+
+        # Given an integer that is more than 4 bytes
+        n = 1073741826
+
+        # When I call pack_i on the integer
+        # Then it raises ValueError
+        with self.assertRaises(ValueError):
+            pack_i(n)
+
+    def test_pack_unpack_l(self) -> None:
+
+        # Given an long
+        n = 1073741828
+
+        # When I call pack_l on the long
+        # and call unpack_i on the packed long
+        packed_n = pack_l(n)
+        unpacked_n = unpack_l(packed_n)
+
+        # Then unpacked long equals the original long
+        self.assertEqual(unpacked_n, 1073741828)
+
+    def test_pack_unpack_l_too_big(self) -> None:
+
+        # Given an long that is more than 8 bytes
+        n = 9223372036854775808
+
+        # When I call pack_l on the long
+        # Then it raises ValueError
+        with self.assertRaises(ValueError):
+            pack_l(n)
+
+    def test_pack_unpack_b(self) -> None:
+
+        # Given a boolean
+        b = False
+
+        # When I call pack_b on the boolean
+        # and call unpack_b on the packed boolean
+        packed_b = pack_b(b)
+        unpacked_b = unpack_b(packed_b)
+
+        # Then unpacked boolean equals the original boolean
+        self.assertFalse(unpacked_b)
