effc71a53cf08431bfff6ddeba3999940f3fafe9
j--c <johnmichaelcollins@protonmail.com>
2024-11-12 08:17:40 -0500

    updated FileStorage and TestFileStorage to be smaller

--- ornament/storage.py
+++ ornament/storage.py
@@ -1,11 +1,6 @@
 from ornament.helper import open_binary_file
 from io import BufferedIOBase
 from os import SEEK_CUR
-import struct
-
-STR_ENCODING = 'utf-8'
-LNG_MAX = 9223372036854775807
-INT_MAX = 1073741824
 
 
 # TODO - this should inherit from base class
@@ -13,66 +8,24 @@ class FileStorage(object):
 
     _handle: BufferedIOBase
 
+    # initializes and opens a binary file stream
     def __init__(self, path: str) -> None:
         self._handle = open_binary_file(path, 'r+b')
 
+    # closes the binary file stream
     def close(self) -> None:
         self._handle.close()
 
+    # read bytes of specified len (dlen) from the position and whence specified
     def read(self, dlen: int, pos: int = 0, whence: int = SEEK_CUR) -> bytes:
         self._handle.seek(pos, whence)
         return self._handle.read(dlen)
 
+    # write bytes of data to the position and whence specified
     def write(self, data: bytes, pos: int = 0, whence: int = SEEK_CUR) -> int:
         self._handle.seek(pos, whence)
         return self._handle.write(data)
 
-    # Strings -----------------------------------------
-    def read_s(self, dlen: int, pos: int = 0, whence: int = SEEK_CUR) -> str:
-        pack_format = f'@{dlen}s'
-        data = self.read(dlen, pos, whence)
-        s_encoded = struct.unpack(pack_format, data)[0]
-        return s_encoded.decode(STR_ENCODING)
-
-    def write_s(self, s: str, pos: int = 0, whence: int = SEEK_CUR) -> int:
-        s_encoded = s.encode(STR_ENCODING)
-        s_len = len(s_encoded)
-        pack_format = f'@{s_len}s'
-        return self.write(struct.pack(pack_format, s_encoded), pos, whence)
-
-    # Integers ----------------------------------------
-    def read_i(self, pos: int = 0, whence: int = SEEK_CUR) -> int:
-        pack_format = '@i'
-        data = self.read(4, pos, whence)
-        return struct.unpack(pack_format, data)[0]
-
-    def write_i(self, n: int, pos: int = 0, whence: int = SEEK_CUR) -> int:
-        pack_format = '@i'
-        if n > INT_MAX:
-            val_msg = f'write_l requires value less than or equal to {LNG_MAX}'
-            raise ValueError(val_msg)
-        return self.write(struct.pack(pack_format, n), pos, whence)
-
-    # Longs -------------------------------------------
-    def read_l(self, pos: int = 0, whence: int = SEEK_CUR) -> int:
-        pack_format = '@l'
-        data = self.read(8, pos, whence)
-        return struct.unpack(pack_format, data)[0]
-
-    def write_l(self, n: int, pos: int = 0, whence: int = SEEK_CUR) -> int:
-        pack_format = '@l'
-        if n > LNG_MAX:
-            return self.write(struct.pack(pack_format, n), pos, whence)
-        else:
-            val_msg = f'write_l requires value less than or equal to {LNG_MAX}'
-            raise ValueError(val_msg)
-
-    # Booleans-----------------------------------------
-    def read_b(self, pos: int = 0, whence: int = SEEK_CUR) -> bool:
-        pack_format = '@?'
-        data = self.read(1, pos, whence)
-        return struct.unpack(pack_format, data)[0]
-
-    def write_b(self, b: bool, pos: int = 0, whence: int = SEEK_CUR) -> int:
-        pack_format = '@?'
-        return self.write(struct.pack(pack_format, b), pos, whence)
+    # return the current position of the cursor in the binary stream
+    def cur_pos(self) -> int:
+        return self._handle.tell()
--- test/test_storage.py
+++ test/test_storage.py
@@ -14,60 +14,59 @@ class TestFileStorage(unittest.TestCase):
             FileStorage(self._dummy_path())
 
     @patch('ornament.storage.open_binary_file')
-    def test_read_write_s_begin_file(self, mock_open) -> None:
-        mock_open.return_value = BytesIO(b'123456789')
-        fs = FileStorage(self._dummy_path())
-        s_to_write = 'begin'
-        fs.write_s(s_to_write, 0, SEEK_SET)
-        s_read = fs.read_s(5, 0, SEEK_SET)
+    def test_read_write_end_file(self, mock_open) -> None:
+
+        # Given a FileStorage
+        mock_open.return_value = self._bytes_io()
+        fs = self._file_storage()
+
+        # When I write to the end of the FileStoraga and
+        # read from the end of the FileStorage
+        fs.write(bytes('str', 'utf-8'), 0, SEEK_END)
+        d_from_fs = fs.read(3, -3, SEEK_END)
         fs.close()
-        self.assertEqual(s_read, s_to_write)
+
+        # Then the value read is equal to the value written
+        self.assertEqual(bytes('str', 'utf-8'), d_from_fs)
 
     @patch('ornament.storage.open_binary_file')
-    def test_read_write_s_end_file(self, mock_open) -> None:
-        mock_open.return_value = BytesIO(b'123456789')
-        fs = FileStorage(self._dummy_path())
-        s_to_write = 'end'
-        fs.write_s(s_to_write, 0, SEEK_END)
-        s_read = fs.read_s(3, -3, SEEK_END)
+    def test_read_write_begin_file(self, mock_open) -> None:
+
+        # Given a FileStorage
+        mock_open.return_value = self._bytes_io()
+        fs = self._file_storage()
+
+        # When I write to the beginning of the FileStorage and
+        # read from the beginning of the FileStorage
+        fs.write(bytes('string', 'utf-8'), 0, SEEK_SET)
+        d_from_fs = fs.read(6, 0, SEEK_SET)
         fs.close()
-        self.assertEqual(s_read, s_to_write)
+
+        # Then the value read is equal to the value written
+        self.assertEqual(bytes('string', 'utf-8'), d_from_fs)
 
     @patch('ornament.storage.open_binary_file')
-    def test_read_write_i_begin_file(self, mock_open) -> None:
-        mock_open.return_value = BytesIO(b'123456789')
-        fs = FileStorage(self._dummy_path())
-        fs.write_i(89, 0, SEEK_SET)
-        i_read = fs.read_i(0, SEEK_SET)
-        fs.close()
-        self.assertEqual(i_read, 89)
+    def test_current_pos(self, mock_open) -> None:
 
-    @patch('ornament.storage.open_binary_file')
-    def test_read_write_i_end_file(self, mock_open) -> None:
-        mock_open.return_value = BytesIO(b'123456789')
-        fs = FileStorage(self._dummy_path())
-        fs.write_i(29789, 0, SEEK_END)
-        i_read = fs.read_i(-4, SEEK_END)
-        fs.close()
-        self.assertEqual(i_read, 29789)
+        # Given a FileStorage
+        mock_open.return_value = self._bytes_io()
+        fs = self._file_storage()
 
-    @patch('ornament.storage.open_binary_file')
-    def test_read_write_b_begin_file(self, mock_open) -> None:
-        mock_open.return_value = BytesIO(b'123456789')
-        fs = FileStorage(self._dummy_path())
-        fs.write_b(False, 0, SEEK_SET)
-        b_read = fs.read_b(0, SEEK_SET)
+        # When I move to the 6th position by writing
+        # a 6 char string
+        fs.write(bytes('string', 'utf-8'), 0, SEEK_SET)
+        cur_pos = fs.cur_pos()
         fs.close()
-        self.assertEqual(b_read, False)
 
-    @patch('ornament.storage.open_binary_file')
-    def test_read_write_b_end_file(self, mock_open) -> None:
-        mock_open.return_value = BytesIO(b'123456789')
-        fs = FileStorage(self._dummy_path())
-        fs.write_b(False, 0, SEEK_END)
-        b_read = fs.read_b(-1, SEEK_END)
-        fs.close()
-        self.assertEqual(b_read, False)
+        # Then the value returned by FileStorage.cur_pos() is 6
+        self.assertEqual(cur_pos, 6)
+
+    # Private -----------------------------------------
+    def _bytes_io(self) -> BytesIO:
+        return BytesIO(b'123456789')
+
+    def _file_storage(self) -> FileStorage:
+        return FileStorage(self._dummy_path)
 
     def _dummy_path(self) -> str:
         return 'dummypath'
